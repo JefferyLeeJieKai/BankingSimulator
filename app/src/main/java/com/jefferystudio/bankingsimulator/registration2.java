c5640f9b818fa057eae1cbe4411a84c0fcc53313
user <ngyewming_0205@yahoo.com.sg>
2019-09-25 11:13:56 +0800

    f

---
+++ app/src/main/java/com/jefferystudio/bankingsimulator/registration2.java
@@ -0,0 +1,11 @@
+package com.jefferystudio.bankingsimulator;
+
+import android.os.Bundle;
+
+public class registration2 {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        new registration2();
+    }
+}
