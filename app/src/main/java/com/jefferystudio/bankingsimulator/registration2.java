0966116151b8cbb189e9ee58007375206bcc5630
user <ngyewming_0205@yahoo.com.sg>
2019-09-28 11:45:19 +0800

    d

--- app/src/main/java/com/jefferystudio/bankingsimulator/registration2.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/registration2.java
@@ -2,12 +2,32 @@ package com.jefferystudio.bankingsimulator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class registration2 extends AppCompatActivity {
+
+
+    private long date;
+    private DateFormat format;
+    String currentDateandTime;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration2);
+
+        //create a date string.
+        String date_n = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
+        //get hold of textview.
+        TextView date  = (TextView) findViewById(R.id.dateview);
+        //set it as current date.
+        date.setText(date_n);
     }
 }
