3fa172c89f7f95d0856d0a975e517996054aaf56
user <ngyewming_0205@yahoo.com.sg>
2019-09-28 11:59:11 +0800

    d

--- app/src/main/java/com/jefferystudio/bankingsimulator/registration2.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/registration2.java
@@ -1,7 +1,11 @@
 package com.jefferystudio.bankingsimulator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -13,10 +17,7 @@ import java.util.Locale;
 
 public class registration2 extends AppCompatActivity {
 
-
-    private long date;
-    private DateFormat format;
-    String currentDateandTime;
+    private Button buttonsubmit;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -29,5 +30,20 @@ public class registration2 extends AppCompatActivity {
         TextView date  = (TextView) findViewById(R.id.dateview);
         //set it as current date.
         date.setText(date_n);
+
+        buttonsubmit =(Button) findViewById(R.id.btnsubmit);
+
+        buttonsubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                MainActivity();
+            }
+        });
+    }
+
+    public void MainActivity(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
