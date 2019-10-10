60af4fa8a0d6e8fa5431701a1c371140aab3b961
PEICHENG <opeixqio@hotmail.com>
2019-10-10 12:19:28 +0800

    .

--- app/src/main/java/com/jefferystudio/bankingsimulator/Settings.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/Settings.java
@@ -9,8 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class Settings extends AppCompatActivity {
+
+    private TextView date;
+    private TextView time;
+    private TextView timezone;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +30,30 @@ public class Settings extends AppCompatActivity {
         setSupportActionBar(homeScreenToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //set date
+        TimeZone t = TimeZone.getTimeZone("Asia/Singapore");
+
+        Calendar cal = Calendar.getInstance(t);
+
+        String strDate = String.format("%s", cal.get(Calendar.DATE));
+
+        date = (TextView) findViewById(R.id.dateLbl);
+        date.setText(strDate);
+
+        //set time
+        String strTime = String.format("%02d" , cal.get(Calendar.HOUR_OF_DAY)) + ":" +
+                String.format("%02d" , cal.get(Calendar.MINUTE)) + ":" +
+                String.format("%02d" , cal.get(Calendar.SECOND));
+
+        time = (TextView) findViewById(R.id.timeLbl);
+        time.setText(strTime);
+
+        //set timezone
+        String strTimezone = t.getDisplayName(false, TimeZone.SHORT) + " - " +
+                             t.getID();
+
+        timezone = (TextView) findViewById(R.id.timezoneLbl);
+        timezone.setText(strTimezone);
     }
 
     @Override
