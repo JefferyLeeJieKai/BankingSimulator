729b887fa32bccfa97790f63fc1b460d8f565018
porkbacon <36147786+porkbacon@users.noreply.github.com>
2019-11-06 21:06:09 +0800

    quiz in progress

--- app/src/main/java/com/jefferystudio/bankingsimulator/Quiz/QuizActivity.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/Quiz/QuizActivity.java
@@ -1,11 +1,12 @@
 package com.jefferystudio.bankingsimulator.Quiz;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.CountDownTimer;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -19,7 +20,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class QuizActivity extends AppCompatActivity {
-
+    public static final String EXTRA_SCORE = "extraScore";
     private static final long COUNTDOWN_IN_MILLIS = 30000;
 
     private TextView textViewQuestion;
@@ -31,6 +32,9 @@ public class QuizActivity extends AppCompatActivity {
     private RadioButton rb3;
     private Button buttonConfirmNext;
 
+    private ColorStateList textColorDefaultRb;
+    private ColorStateList textColorDefaultCd;
+
     private CountDownTimer countDownTimer;
     private long timeLeftInMillis;
 
@@ -40,12 +44,12 @@ public class QuizActivity extends AppCompatActivity {
     private Question currentQuestion;
 
     private int score;
-
     private boolean answered;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    private long backPressedTime;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
@@ -58,6 +62,9 @@ public class QuizActivity extends AppCompatActivity {
         rb3 = findViewById(R.id.radio_button3);
         buttonConfirmNext = findViewById(R.id.button_confirm_next);
 
+        textColorDefaultRb = rb1.getTextColors();
+        textColorDefaultCd = textViewCountDown.getTextColors();
+
         QuizDbHelper dbHelper = new QuizDbHelper(this);
         questionList = dbHelper.getAllQuestions();
         questionCountTotal = questionList.size();
@@ -66,18 +73,15 @@ public class QuizActivity extends AppCompatActivity {
         showNextQuestion();
 
         buttonConfirmNext.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 if (!answered) {
                     if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked()) {
                         checkAnswer();
-                    }
-                    else {
+                    } else {
                         Toast.makeText(QuizActivity.this, "Please select an answer", Toast.LENGTH_SHORT).show();
                     }
-                }
-                else {
+                } else {
                     showNextQuestion();
                 }
             }
@@ -85,29 +89,29 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void showNextQuestion() {
+        rb1.setTextColor(textColorDefaultRb);
+        rb2.setTextColor(textColorDefaultRb);
+        rb3.setTextColor(textColorDefaultRb);
         rbGroup.clearCheck();
 
-        if (questionCounter < questionCountTotal) {
+        if (questionCounter < 5) {
             currentQuestion = questionList.get(questionCounter);
 
-        textViewQuestion.setText(currentQuestion.getQuestion());
-        rb1.setText(currentQuestion.getOption1());
-        rb2.setText(currentQuestion.getOption2());
-        rb3.setText(currentQuestion.getOption3());
+            textViewQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
 
-        questionCounter++;
-        textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            questionCounter++;
+            textViewQuestionCount.setText("Question: " + questionCounter + "/" + "5");
+            answered = false;
+            buttonConfirmNext.setText("Confirm");
 
-        timeLeftInMillis = COUNTDOWN_IN_MILLIS;
-        startCountDown();
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
         } else {
             finishQuiz();
         }
-    }
-
-    private void finishQuiz() {
-
-        finish();
     }
 
     private void startCountDown() {
@@ -128,12 +132,17 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateCountDownText() {
-        int minutes = (int) (timeLeftInMillis / 1000) / 60;
         int seconds = (int) (timeLeftInMillis / 1000) % 60;
 
-        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        String timeFormatted = String.format(Locale.getDefault(), "%02d" , seconds);
 
         textViewCountDown.setText(timeFormatted);
+
+        if (timeLeftInMillis < 10000) {
+            textViewCountDown.setTextColor(Color.RED);
+        } else {
+            textViewCountDown.setTextColor(textColorDefaultCd);
+        }
     }
 
     private void checkAnswer() {
@@ -145,12 +154,55 @@ public class QuizActivity extends AppCompatActivity {
         int answerNr = rbGroup.indexOfChild(rbSelected) + 1;
 
         if (answerNr == currentQuestion.getAnswerNr()) {
-           score++;
+            score++;
         }
 
-        if (questionCounter == questionCountTotal) {
+        showSolution();
+    }
+
+    private void showSolution() {
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNr()) {
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 1 is correct");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 2 is correct");
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 3 is correct");
+                break;
+        }
+
+        if (questionCounter < 5) {
+            buttonConfirmNext.setText("Next");
+        } else {
             buttonConfirmNext.setText("Finish");
         }
+    }
+
+    private void finishQuiz() {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            finishQuiz();
+        } else {
+            Toast.makeText(this, "Press back again to end", Toast.LENGTH_SHORT).show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
     }
 
     @Override
