dfae6c43ea0c986d62e961be12e121fb8c02fccc
PEICHENG <opeixqio@hotmail.com>
2019-11-07 17:56:21 +0800

    .

--- app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/CreateClass.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/CreateClass.java
@@ -1,19 +1,50 @@
 package com.jefferystudio.bankingsimulator.BankerManageAccount;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.jefferystudio.bankingsimulator.R;
 
 public class CreateClass extends Fragment {
 
+    TextInputLayout getClass;
+    TextInputLayout interest;
+    Button createButton;
+    Button cancelButton;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.class_create, container, false);
 
+        getClass = view.findViewById(R.id.classTxt);
+        interest = view.findViewById(R.id.intRateTxt);
+
+        //create button
+        createButton = view.findViewById(R.id.createBtn);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //do something
+
+            }
+        });
+
+        //cancel button
+        cancelButton = view.findViewById(R.id.cancelBtn);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //do something
+
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/EditClass.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/EditClass.java
@@ -1,19 +1,73 @@
 package com.jefferystudio.bankingsimulator.BankerManageAccount;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.jefferystudio.bankingsimulator.R;
 
 public class EditClass extends Fragment {
 
+    TextInputLayout searchClass;
+    Button searchButton;
+    TextView classLabel;
+    Button editClassButton;
+    TextView interest;
+    Button editIntButton;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.class_edit, container, false);
 
+        //default
+        classLabel = view.findViewById(R.id.classLbl);
+        classLabel.setText("NIL");
+
+        interest = view.findViewById(R.id.intRateLbl);
+        interest.setText("NIL");
+
+        searchClass = view.findViewById(R.id.classTxt);
+
+        //search button
+        searchButton = view.findViewById(R.id.searchBtn);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //draw out information from database
+                //testing purpose
+                classLabel.setText("1");
+                interest.setText("0.2");
+
+            }
+        });
+
+        //edit class button
+        editClassButton = view.findViewById(R.id.editClass);
+        editClassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //do something
+
+            }
+        });
+
+        //edit interest button
+        editIntButton = view.findViewById(R.id.editIntRate);
+        editIntButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //do something
+
+            }
+        });
 
         return view;
     }
