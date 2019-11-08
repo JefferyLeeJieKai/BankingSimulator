11885caa8879786c20fa1504a7ab20d6c96c3902
PEICHENG <opeixqio@hotmail.com>
2019-11-08 16:08:58 +0800

    update: validation

--- app/src/main/java/com/jefferystudio/bankingsimulator/ViewTransactionsPackage/ViewTransactionsBanker.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/ViewTransactionsPackage/ViewTransactionsBanker.java
@@ -1,19 +1,42 @@
 package com.jefferystudio.bankingsimulator.ViewTransactionsPackage;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.jefferystudio.bankingsimulator.R;
+import com.jefferystudio.bankingsimulator.Validation;
 
 public class ViewTransactionsBanker extends Fragment {
+
+    private TextInputLayout username;
+    private Button searchButton;
+    String input;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.viewtransactions_banker, container, false);
 
+        username = view.findViewById(R.id.usernameTxt);
+
+        //search button
+        searchButton = view.findViewById(R.id.searchBtn);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                input = username.getEditText().getText().toString().trim();
+
+                //if not empty
+                if (Validation.validateEmpty(input, username)) {
+                    username.setError(null);
+                }
+            }
+        });
 
         return view;
     }
