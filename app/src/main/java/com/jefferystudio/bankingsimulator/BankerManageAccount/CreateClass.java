77499cdef7b0bde5e47d1b58d3044bdf337d64b9
PEICHENG <opeixqio@hotmail.com>
2019-11-08 15:46:17 +0800

    update: validation

--- app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/CreateClass.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/CreateClass.java
@@ -1,38 +1,78 @@
 package com.jefferystudio.bankingsimulator.BankerManageAccount;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.jefferystudio.bankingsimulator.LoginAndHomepagePackage.HomeFragmentBanker;
-import com.jefferystudio.bankingsimulator.LoginAndHomepagePackage.HomeScreenBanker;
 import com.jefferystudio.bankingsimulator.R;
-import com.jefferystudio.bankingsimulator.Transfer_Amount;
-import com.jefferystudio.bankingsimulator.goalspage;
+import com.jefferystudio.bankingsimulator.Validation;
 
 public class CreateClass extends Fragment {
 
-    TextInputLayout getClass;
-    TextInputLayout interest;
-    private Button CancelBtn;
+    private TextInputLayout searchClass;
+    private TextInputLayout interest;
+    private Button createButton;
+    private Button cancelButton;
     private Bundle args;
+    private String inputClass, inputInt;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.class_create, container, false);
 
-        getClass = view.findViewById(R.id.classTxt);
+        searchClass = view.findViewById(R.id.classTxt);
         interest = view.findViewById(R.id.intRateTxt);
-        CancelBtn = view.findViewById(R.id.cancelBtn);
+
+        //create button
+        createButton = view.findViewById(R.id.createBtn);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //if invalid class or interest rate found
+                if (!validateClass() | !validateInt()) {
+
+                    return;
+                }
+            }
+        });
+
+        //cancel button
+        cancelButton = view.findViewById(R.id.cancelBtn);
 
 
         return view;
+    }
+
+    private boolean validateClass() {
+
+        inputClass = searchClass.getEditText().getText().toString().trim();
+
+        boolean result = Validation.validateEmpty(inputClass, searchClass);
+
+        if (result) {
+            searchClass.setError(null);
+        }
+
+        return result;
+    }
+
+    private boolean validateInt() {
+
+        inputInt = interest.getEditText().getText().toString().trim();
+
+
+        boolean result = Validation.validateAmount(inputInt, interest);
+
+        if (result) {
+            interest.setError(null);
+        }
+
+        return result;
     }
 }
