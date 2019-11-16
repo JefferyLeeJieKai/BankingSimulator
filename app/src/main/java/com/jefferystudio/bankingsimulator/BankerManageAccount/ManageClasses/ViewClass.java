a51b50599d01095028699816329b6afc75b2e31f
user <ngyewming_0205@yahoo.com.sg>
2019-11-16 13:07:54 +0800

    h

--- app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/ManageClasses/ViewClass.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/BankerManageAccount/ManageClasses/ViewClass.java
@@ -2,13 +2,18 @@ package com.jefferystudio.bankingsimulator.BankerManageAccount.ManageClasses;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.Button;
 
 import com.jefferystudio.bankingsimulator.BankerManageAccount.ManageClasses.ViewClassRecyclerView.ClassViewRecyclerViewAdaptor;
+import com.jefferystudio.bankingsimulator.DepositPackage.DepositAH;
+import com.jefferystudio.bankingsimulator.LoginAndHomepagePackage.HomeFragmentBanker;
+import com.jefferystudio.bankingsimulator.LoginAndHomepagePackage.HomeFragmentUser;
 import com.jefferystudio.bankingsimulator.R;
 
 import java.util.ArrayList;
@@ -17,6 +22,8 @@ public class ViewClass extends Fragment {
 
     private Bundle args;
     private RecyclerView classDetails;
+    private Button buttonback;
+
     private ArrayList<Class> classList;
 
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +35,19 @@ public class ViewClass extends Fragment {
         classDetails = view.findViewById(R.id.classDetailsRv);
         new ClassAsync(getActivity(), "ViewClass", args.getString("userID"), null, null, classDetails).execute(args.getString("userID"));
 
+        buttonback = view.findViewById(R.id.buttonback);
+
+        buttonback.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Fragment homeFragmentBanker = new HomeFragmentBanker();
+                homeFragmentBanker.setArguments(args);
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, homeFragmentBanker);
+                transaction.commit();
+            }
+        });
         return view;
     }
 
