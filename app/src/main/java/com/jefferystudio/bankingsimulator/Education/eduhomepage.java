e04125dcc7ac83af7443364d89fe54f7eadefe46
user <ngyewming_0205@yahoo.com.sg>
2019-11-19 11:01:30 +0800

    h

--- app/src/main/java/com/jefferystudio/bankingsimulator/Education/eduhomepage.java
+++ app/src/main/java/com/jefferystudio/bankingsimulator/Education/eduhomepage.java
@@ -1,15 +1,22 @@
 package com.jefferystudio.bankingsimulator.Education;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
+import com.jefferystudio.bankingsimulator.LoginAndHomepagePackage.HomeScreenUser;
 import com.jefferystudio.bankingsimulator.R;
 
 import java.util.Vector;
 
 public class eduhomepage extends AppCompatActivity {
+
+    private Button backbutton;
+    private Bundle args;
 
     RecyclerView recyclerView;
     Vector<YoutubeVideo> youtubeVideos = new Vector<>();
@@ -18,6 +25,8 @@ public class eduhomepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.education_video);
+
+        args = getIntent().getExtras();
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -35,5 +44,20 @@ public class eduhomepage extends AppCompatActivity {
         VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);
 
         recyclerView.setAdapter(videoAdapter);
+
+        backbutton = (Button) findViewById(R.id.btnback);
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Bundle newArgs = new Bundle();
+                newArgs.putString("userID", args.getString("userID"));
+                newArgs.putString("userName", args.getString("userName"));
+                Intent intent = new Intent(getApplicationContext(), HomeScreenUser.class);
+                intent.putExtras(newArgs);
+                startActivity(intent);
+            }
+        });
     }
 }
