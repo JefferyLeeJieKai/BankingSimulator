47722b02c0e2ffff9228b4c98223d0392b36770f
JefferyLeeJieKai <36294848+JefferyLeeJieKai@users.noreply.github.com>
2019-11-20 22:30:27 +0800

    20/11/2019 Version

---
+++ app/src/main/java/com/jefferystudio/bankingsimulator/Quiz/UpdateScoreAsync.java
@@ -0,0 +1,83 @@
+package com.jefferystudio.bankingsimulator.Quiz;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+public class UpdateScoreAsync extends AsyncTask <String, String, String>{
+
+    private Context context;
+    private ProgressDialog progDialog;
+
+    public UpdateScoreAsync(Context context) {
+
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+        progDialog = new ProgressDialog(context);
+        progDialog.setMessage("Uploading score to database...");
+        progDialog.setIndeterminate(false);
+        progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progDialog.setCancelable(false);
+        progDialog.show();
+    }
+
+    @Override
+    protected String doInBackground(String[] args) {
+
+        StringBuffer sb = new StringBuffer("");
+
+        String currentUserID = args[0];
+        String currentScore = args[1];
+
+        try {
+
+            String link = "https://www.kidzsmartapp.com/databaseAccess/uploadQuizScore";
+            String data = URLEncoder.encode("userid", "UTF-8") + "=" +
+                    URLEncoder.encode(currentUserID, "UTF-8");
+            data += "&" + URLEncoder.encode("score", "UTF-8") + "=" +
+                    URLEncoder.encode(currentScore, "UTF-8");
+
+            URL url = new URL(link);
+            URLConnection connection = url.openConnection();
+
+            connection.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+
+            wr.write(data);
+            wr.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line = null;
+
+            // Read Server Response
+            while((line = reader.readLine()) != null) {
+                sb.append(line);
+                break;
+            }
+        }
+        catch(IOException e) {
+
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        progDialog.dismiss();
+    }
+}
